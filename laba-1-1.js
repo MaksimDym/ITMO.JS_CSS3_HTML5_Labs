195d38b4f5ae7c32803dfc7349e7cb2523ec4fe9
MaksimDym <fanmaxon@yandex.ru>
2025-03-26 20:53:59 +0300

    Add files via upload

---
+++ laba-1-1.js
@@ -0,0 +1,10 @@
+var string1 = "Привет, ";
+var string2 = "мир!";
+var combinedString = string1 + string2;
+
+let pi = 3.14;
+let FF = 2; 
+let product = pi * FF;
+
+alert("Объединенная строка: " + combinedString);
+alert("Произведение 3.14 и " + FF + ": " + product);
